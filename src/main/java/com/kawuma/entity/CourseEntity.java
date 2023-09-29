7c519816c97af4a3e0f44948a33d83c6a5594c44
skawuma <s_kawuma@salemstate.edu>
2023-09-29 18:58:28 -0400

    made changes to DTO

--- src/main/java/com/kawuma/entity/CourseEntity.java
+++ src/main/java/com/kawuma/entity/CourseEntity.java
@@ -1,10 +1,7 @@
 package com.kawuma.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
